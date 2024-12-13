ecf307320532a91a3bb17c9727a7ae51f573b9fc
guna-sd <sdgunaseker@gmail.com>
2024-12-13 21:33:11 +0530

    Modified StdTypeDialect.h and removed obsolete unwanted files

--- include/StdType/IR/StdTypeDialect.h
+++ include/StdType/IR/StdTypeDialect.h
@@ -1,34 +1,51 @@
-//===- StdTypeDialect.h - StdType Dialect -----------------------*- C++ -*-===//
+/*===- TableGen'erated file -------------------------------------*- C++ -*-===*\
+|*                                                                            *|
+|* Dialect Declarations                                                       *|
+|*                                                                            *|
+|* Automatically generated file, do not edit!                                 *|
+|* From: StdTypeDialect.td                                                    *|
+|*                                                                            *|
+\*===----------------------------------------------------------------------===*/
 
 
-
-//===----------------------------------------------------------------------===//
-
-
-#ifndef MLIR_DIALECT_STDTYPE_IR_STDTYPEDIALECT_H
-#define MLIR_DIALECT_STDTYPE_IR_STDTYPEDIALECT_H
-
-#include "mlir/IR/Types.h"
-#include "mlir/IR/Operation.h"
-#include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
-#include "mlir/IR/OpDefinition.h"
-#include "mlir/IR/TypeSupport.h"
-#include "mlir/IR/TypeUtilities.h"
 
 namespace mlir {
-namespace stdtype {
+namespace StdType {
 
-class IndexType;
-class IntegerType;
-class FloatType;
-class BoolType;
-class StringType;
-class VectorType;
-class ArrayType;
+class StdTypeDialect : public ::mlir::Dialect {
+  explicit StdTypeDialect(::mlir::MLIRContext *context);
 
-}
-}
+  void initialize();
+  friend class ::mlir::MLIRContext;
+public:
+  ~StdTypeDialect() override;
+  static constexpr ::llvm::StringLiteral getDialectNamespace() {
+    return ::llvm::StringLiteral("stdtype");
+  }
 
+  /// Parse an attribute registered to this dialect.
+  ::mlir::Attribute parseAttribute(::mlir::DialectAsmParser &parser,
+                                   ::mlir::Type type) const override;
 
-#endif
+  /// Print an attribute registered to this dialect.
+  void printAttribute(::mlir::Attribute attr,
+                      ::mlir::DialectAsmPrinter &os) const override;
+
+  /// Materialize a single constant operation from a given attribute value with
+  /// the desired resultant type.
+  ::mlir::Operation *materializeConstant(::mlir::OpBuilder &builder,
+                                         ::mlir::Attribute value,
+                                         ::mlir::Type type,
+                                         ::mlir::Location loc) override;
+
+    static bool isCompatibleType(Type);
+
+    Type parseType(DialectAsmParser &p) const override;
+    void printType(Type, DialectAsmPrinter &p) const override;
+    
+    void registerTypes();
+  };
+} // namespace StdType
+} // namespace mlir
+MLIR_DECLARE_EXPLICIT_TYPE_ID(::mlir::StdType::StdTypeDialect)
